4d06efc573f8a7466b91b8aab7801514914225d4
brandonocasey <brandonocasey@gmail.com>
2018-01-05 15:51:21 -0500

    fix: let the tech preload auto on its own

--- src/js/player.js
+++ src/js/player.js
@@ -2471,11 +2471,7 @@ class Player extends Component {
         this.techCall_('src', source.src);
       }
 
-      if (this.options_.preload === 'auto') {
-        this.load();
-      }
-
-    // Set the source synchronously if possible (#2326)
+      // Set the source synchronously if possible (#2326)
     }, true);
 
     return false;
